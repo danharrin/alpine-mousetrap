f56fcafab9dad4fd3418ccd32ead8b5c7e89d0e4
Sebastian Popp <git@sebastianpopp.com>
2024-12-09 18:14:54 +0100

    Replace double dashes by spaces

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ export default (Alpine) => {
     Alpine.directive('mousetrap', (el, { modifiers, expression }, { evaluate }) => {
         const action = () => expression ? evaluate(expression) : el.click()
 
-        modifiers = modifiers.map((modifier) => modifier.replace(/-/g, '+'))
+        modifiers = modifiers.map((modifier) => modifier
+            .replace(/--/g, ' ')
+            .replace(/-/g, '+')
+        );
 
         if (modifiers.includes('global')) {
             modifiers = modifiers.filter((modifier) => modifier !== 'global')
